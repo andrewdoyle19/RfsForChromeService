980bac0d245f1381034ae21e35c750d89f432d1e
Andrew Doyle <andrew.doyle.83@gmail.com>
2013-11-09 14:47:05 +1100

    rename module

---
+++ src/RfsForChrome.Service/FireSearchModule.cs
@@ -0,0 +1,12 @@
+﻿using Ninject.Modules;
+
+namespace RfsForChrome.Service
+{
+    public class MyModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IFetchMajorIncidents>().To<FetchMajorIncidents>().InSingletonScope();
+        }
+    }
+}
